a41e5bced67d07924694c55c3bad7b92c530afcf
Binh Trinh <trinhthaibinh.ecom@gmail.com>
2021-05-10 17:24:36 -0700

    fix cookie domain

--- src/pages/api/logout.ts
+++ src/pages/api/logout.ts
@@ -8,6 +8,7 @@ export default withSession(async (req: any, res: any) => {
   cookies.set('jwt', 'loggedout', {
     expires: new Date(Date.now() + 10 * 1000),
     httpOnly: true,
+    domain: 'https://polar-river-10904.herokuapp.com/',
   });
   res.json({ status: 'success' });
 });
