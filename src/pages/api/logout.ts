dc08947f14e293ec427b95f90fd637c0949dcb77
Binh Trinh <trinhthaibinh.ecom@gmail.com>
2021-05-14 16:51:26 -0700

    try unset session

--- src/pages/api/logout.ts
+++ src/pages/api/logout.ts
@@ -2,12 +2,13 @@ import Cookies from 'cookies';
 import { withSession } from '../../lib/withSession';
 
 export default withSession(async (req: any, res: any) => {
-  req.session.destroy();
+  req.session.unset('jwt');
+  await req.session.save();
 
   const cookies = new Cookies(req, res);
   cookies.set('jwt', 'loggedout', {
     expires: new Date(Date.now() + 10 * 1000),
-    httpOnly: true,
+    httpOnly: false,
     domain:
       process.env.NODE_ENV === 'production' ? '.talaria-order.xyz' : undefined,
   });
